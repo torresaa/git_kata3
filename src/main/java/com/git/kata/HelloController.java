f871872fdadf435d3dcd47e5be90e3bc71fee8d6
Aquiles TORRES <aquiles.torres@meritis.fr>
2023-05-10 00:25:08 +0200

    save html
    adding dependency
    
    resolve git conflicts
    
    fix missing file

--- src/main/java/com/git/kata/HelloController.java
+++ src/main/java/com/git/kata/HelloController.java
@@ -1,14 +1,20 @@
 package com.git.kata;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
+	@Value("${spring.application.name}")
+	String appName;
 	@GetMapping("/")
+
 	public String index() {
-		return "Hello Meritis Git Kata Follower!";
+		model.addAttribute("appName", appName);
+		return "index";
 	}
 
 }
