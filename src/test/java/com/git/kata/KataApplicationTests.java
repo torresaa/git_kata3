e7579bd629e1e90b2e0eb0b1276efc4322846b21
Aquiles TORRES <aquiles.torres@meritis.fr>
2023-05-09 23:14:03 +0200

    First commit, init SpringBoot project

---
+++ src/test/java/com/git/kata/KataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
