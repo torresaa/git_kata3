f636d9106ecf368ff428d15117cab5058fac53e7
Aquiles TORRES <aquiles.torres@meritis.fr>
2023-05-11 13:27:19 +0200

    Updating test

--- src/test/java/com/git/kata/KataApplicationTests.java
+++ src/test/java/com/git/kata/KataApplicationTests.java
@@ -28,7 +28,7 @@ class KataApplicationTests {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hello World!")));
+				.andExpect(content().string(equalTo("Hello World!2")));
 	}
 
 }
