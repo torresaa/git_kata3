06690775ba567141345627bc7982975daab6e882
Aquiles TORRES <aquiles.torres@meritis.fr>
2023-05-11 07:38:32 +0200

    mocking server

--- src/test/java/com/git/kata/KataApplicationTests.java
+++ src/test/java/com/git/kata/KataApplicationTests.java
@@ -1,13 +1,31 @@
 package com.git.kata;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class KataApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
 
 }
